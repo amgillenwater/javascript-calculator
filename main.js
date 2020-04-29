06ca301aeffbfd258f23aca906174d48cd8d1924
amgillenwater <amgillenwater@gmail.com>
2020-04-29 18:31:58 -0400

    grid in css finished, a start to the js

---
+++ main.js
@@ -0,0 +1,8 @@
+let calculator = document.querySelector(".calculator");
+console.log(calculator)
+
+let clear = document.querySelector("#c");
+clear.addEventListening("click", function(){
+    
+})
+
